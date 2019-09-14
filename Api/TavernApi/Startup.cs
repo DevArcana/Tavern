089e265db1ebe396cf626cff488666b1a0869be0
Jadw1 <jadw1_1@o2.pl>
2019-09-14 13:55:42 +0200

    add jwt token

--- Api/TavernApi/Startup.cs
+++ Api/TavernApi/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TavernApi.Databases;
 
 namespace TavernApi
@@ -32,6 +36,29 @@ namespace TavernApi
       services.AddIdentity<IdentityUser, IdentityRole>()
         .AddEntityFrameworkStores<TavernContext>()
         .AddDefaultTokenProviders();
+
+      JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+      services
+          .AddAuthentication(options =>
+          {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+          })
+          .AddJwtBearer(cfg =>
+          {
+            cfg.RequireHttpsMetadata = false;
+            cfg.SaveToken = true;
+            cfg.TokenValidationParameters = new TokenValidationParameters
+            {
+              ValidIssuer = Configuration["JwtIssuer"],
+              ValidAudience = Configuration["JwtIssuer"],
+              IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+              ClockSkew = TimeSpan.Zero // remove delay of token when expire
+            };
+          });
+
 
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
