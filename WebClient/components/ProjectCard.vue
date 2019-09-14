eb096cf6beca9c77bc8284e0c8e9796ad36b391f
Neferius Leviathanis <neferius.leviathanis@gmail.com>
2019-09-14 17:42:03 +0200

    Add veil to overflowing project card

--- WebClient/components/ProjectCard.vue
+++ WebClient/components/ProjectCard.vue
@@ -17,9 +17,21 @@
     </header>
     <div class="card-content">
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-        <br>
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem consequatur quidem nesciunt nihil soluta eos dignissimos nostrum optio aperiam, laboriosam laudantium at alias veniam quae sunt accusantium porro distinctio est.
+          Officiis, provident. Hic facilis doloribus perspiciatis ab debitis at, error, vel eligendi fuga enim voluptate voluptatibus ea ut recusandae iste natus, dicta blanditiis? Provident ratione facilis similique. Quod, ipsam autem!
+          Explicabo illo accusantium, at delectus harum assumenda unde voluptas quos vitae aperiam? Ipsam qui distinctio illum, nihil, cupiditate reiciendis provident eius possimus quis iure aspernatur eligendi quas sint deserunt nam.
+          Non iure deserunt vel necessitatibus tempore, ipsum explicabo ad ut iusto assumenda distinctio quis ea, est qui aperiam esse voluptates reprehenderit molestias labore, minus quo! Mollitia at velit provident voluptatum.
+          Harum necessitatibus dicta aspernatur laudantium modi eum nesciunt pariatur qui quas molestiae quam dolorum laboriosam, vel veritatis aliquam, excepturi voluptatibus fugit eligendi id minus? Necessitatibus quidem temporibus veniam iste magni?
+          Iste magnam accusantium eum temporibus tempore provident sit aliquam praesentium deleniti expedita minima quos molestiae, aliquid cupiditate est blanditiis voluptatem commodi beatae, inventore quia. At nisi atque dolor vitae adipisci!
+          Exercitationem soluta, laudantium rem repellendus doloremque, cumque fugit, debitis aliquam eos iusto saepe sed. Tempora maiores sit aliquam eaque harum necessitatibus voluptatem vero tempore facere, voluptate, nostrum culpa magnam ad?
+          Iste mollitia repudiandae tempora, nesciunt doloremque accusamus perferendis labore harum. Facere cupiditate blanditiis id reprehenderit dolorem aut exercitationem culpa doloribus. Voluptatem, ex illum maxime praesentium obcaecati sapiente quidem eligendi dolorum.
+          Qui nemo, voluptates iusto tenetur delectus ut quidem repellat neque et autem officiis necessitatibus odit excepturi assumenda fuga, quam ducimus, beatae vel dicta? Voluptas pariatur, esse cupiditate dolor excepturi quo!
+          Rerum, quibusdam! Ea, error. Voluptas doloremque quae modi quas. Cum explicabo, debitis repudiandae nemo ipsa qui voluptas nam quos illo rerum, excepturi animi sunt accusamus nesciunt tempora architecto dignissimos earum!
+          Neque atque quisquam quam ab debitis nam dolorum magnam veniam mollitia reiciendis. Illo cumque aspernatur perferendis blanditiis, incidunt consectetur assumenda, odit quo corrupti ipsam consequatur, saepe unde nobis aperiam expedita!
+          Qui perspiciatis vitae sapiente labore accusantium molestias ut quod id ullam nobis doloribus tempore dolorum a corrupti et facere sunt totam ab pariatur hic illo, nisi explicabo incidunt! Laboriosam, corporis.
+        </div>
+        <div class="veil"></div>
       </div>
     </div>
     <footer class="card-footer">
@@ -37,5 +49,22 @@ export default {
 <style scoped>
 .card-header-title {
   padding-bottom: 0;
+}
+
+.content {
+  max-height: 120px;
+  position: relative;
+  overflow: hidden;
+}
+.content .veil { 
+  position: absolute; 
+  bottom: 0; 
+  left: 0;
+  width: 100%; 
+  text-align: center; 
+  margin: 0; padding: 30px 0; 
+	
+  /* "transparent" only works here because == rgba(0,0,0,0) */
+  background-image: linear-gradient(to bottom, transparent, white);
 }
 </style>
