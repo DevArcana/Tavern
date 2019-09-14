aed13a00335b5c235fb475c9ce9f9695ec0c5ba1
Jadw1 <jadw1_1@o2.pl>
2019-09-14 15:26:32 +0200

    add cors policy

--- Api/TavernApi/Startup.cs
+++ Api/TavernApi/Startup.cs
@@ -31,6 +31,16 @@ namespace TavernApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CORS", builder =>
+        {
+          builder.AllowAnyOrigin();
+          builder.AllowAnyHeader();
+          builder.AllowAnyMethod();
+        });
+      });
+
       services.AddDbContext<TavernContext>();
 
       services.AddIdentity<IdentityUser, IdentityRole>()
