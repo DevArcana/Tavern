74065edba2ce479294e0d316882a184af490fb0e
Jadw1 <jadw1_1@o2.pl>
2019-09-14 15:15:31 +0200

    Add models(WIP)

---
+++ Api/TavernApi/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TavernApi.Models
+{
+  public class Project
+  {
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public virtual Category Category { get; set; }
+  }
+}
