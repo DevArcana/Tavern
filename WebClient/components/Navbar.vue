a2504c77695dc973fe64a136cfbfa324259ecf57
Scytlee <scytlee@disroot.org>
2019-09-14 22:53:56 +0200

    modify navbar

--- WebClient/components/Navbar.vue
+++ WebClient/components/Navbar.vue
@@ -1,23 +1,40 @@
 <template>
-  <b-navbar class="is-dark fixed-top">
-    <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img class="logo"
+  <nav class="navbar is-dark fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img class="logo-desktop is-hidden-touch"
           src="~/assets/logo.png"
           alt="Tavern Social Platform"
         />
-      </b-navbar-item>
-    </template>
+      </a>
+      <a class="navbar-item">
+        <img class="logo-mobile is-hidden-desktop"
+          src="~/assets/logo.png"
+          alt="Tavern Social Platform"
+        />
+      </a>
 
-    <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <nuxt-link class="button is-primary" to="/register">Sign up</nuxt-link>
-          <nuxt-link class="button" to="/login">Log in</nuxt-link>
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <nuxt-link class="button is-primary" to="/register">Sign up</nuxt-link>
+            <nuxt-link class="button" to="/login">Log in</nuxt-link>
+          </div>
         </div>
-      </b-navbar-item>
-    </template>
-  </b-navbar>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -25,16 +42,12 @@ export default {};
 </script>
 
 <style>
-.navbar {
-  height: 5rem;
+.logo-desktop {
+  height: 48px;
+  width: 240px;
 }
-
-.navbar-item, .navbar-brand, .navbar-burger {
-  height: 100%;
-}
-
-.logo {
-  height: 4rem;
-  width: auto;
+.logo-mobile {
+  height: 30px;
+  width: 150px;
 }
 </style>
