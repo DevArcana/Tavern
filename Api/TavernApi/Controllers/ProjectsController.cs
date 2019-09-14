29979ff56acb2e58187c52a12d1bc0682976f1e8
Jadw1 <jadw1_1@o2.pl>
2019-09-15 00:47:36 +0200

    get projects

---
+++ Api/TavernApi/Controllers/ProjectsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TavernApi.Databases;
+using TavernApi.Helpers;
+using TavernApi.Models;
+
+namespace TavernApi.Controllers
+{
+  [ApiController]
+  [Route("api/projects")]
+  public class ProjectsController : Controller
+  {
+    private readonly TavernContext _context;
+
+    public ProjectsController(TavernContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Project>>> GetProjects(int pageSize, int page = 1)
+    {
+      var projects = _context.Projects.GetPaged(page, pageSize);
+
+      return await Task.FromResult(new OkObjectResult(projects.Select(p => new ProjectDTO(p))));
+    }
+
+  }
+}
