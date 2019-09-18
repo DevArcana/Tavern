5f487e75676e9c8edadc709d1036ababcefa67d5
Jadw1 <jadw1_1@o2.pl>
2019-09-18 18:10:40 +0200

    idetntity model config

--- Api/TavernApi/Models/Category.cs
+++ Api/TavernApi/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace TavernApi.Models
+﻿using System.Collections.Generic;
+
+namespace TavernApi.Models
 {
   public class Category
   {
     public long Id { get; set; }
     public string Name { get; set; }
+    public IEnumerable<Project> Projects { get; set; }
   }
 }
