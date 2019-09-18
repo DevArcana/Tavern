c5d15193cdb0275de4e3ac2f84af5cd68a2fe9d1
Jadw1 <jadw1_1@o2.pl>
2019-09-18 10:59:39 +0200

    more models

---
+++ Api/TavernApi/Models/ApplicationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TavernApi.Models.Identity;
+
+namespace TavernApi.Models
+{
+  public class ApplicationRequest
+  {
+    public long Id { get; set; }
+    public long ProjectId { get; set; }
+    public User Applicant { get; set; }
+    public Function Function { get; set; }
+    public bool? Approved { get; set; } = null;
+  }
+}
